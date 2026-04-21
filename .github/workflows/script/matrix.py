a655843b6a8f0fe9c42022a94fba6825561e74ac
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2026-04-21 20:30:57 +0800

    Refactor matrix generation

--- .github/workflows/script/matrix.py
+++ .github/workflows/script/matrix.py
@@ -1,15 +1,18 @@
 from enum import StrEnum, auto
 from pathlib import Path
 from github import Github, Auth
+from semantic_version import Version
 
-import re
 import os
+import re
 import json
 import tomllib
+
 
 class PlatForm(StrEnum):
     MODRINTH = auto()
     CURSEFORGE = auto()
+
 
 class DataType(StrEnum):
     MC_VERS = auto()
@@ -18,103 +21,126 @@ class DataType(StrEnum):
     VER_MAP = auto()
     PUBLISH = auto()
 
-class __CACHE(StrEnum):
+    CREATE = auto()
+    SHOULD_CREATE = auto()
+
+
+class Cache(StrEnum):
     TAGS = auto()
     ASSETS = auto()
 
-CACHE = {
-    __CACHE.TAGS: list(),
-    __CACHE.ASSETS: dict(),
-    'flag': False
-}
 
-def main():
-    create_tag_list = [i for i in get_vers() if not tag_exists(f'release/{i}')]
-    json_data = {
-        'create': create_tag_list,
-        'should_create': len(create_tag_list) != 0,
-        PlatForm.MODRINTH: get_data(PlatForm.MODRINTH),
-        PlatForm.CURSEFORGE: get_data(PlatForm.CURSEFORGE)
-    }
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(json_data, f)
+class Matrix:
+    def __init__(self):
+        self.token: str | None = os.getenv("GH_TOKEN")
+        self.repository: str = os.getenv("GITHUB_REPOSITORY", "DuskScorpio/RedstoneToolkit")
+
+        self.cache = {
+            Cache.TAGS: self.read_tags(),
+            Cache.ASSETS: dict()
+        }
+
+        self.data = {
+            DataType.CREATE: list(),
+            DataType.SHOULD_CREATE: False,
+
+            PlatForm.MODRINTH: {
+                DataType.MC_VERS: list(),
+                DataType.DIR_MAP: dict(),
+                DataType.TYPE_MAP: dict(),
+                DataType.VER_MAP: dict(),
+                DataType.PUBLISH: False
+            },
+
+            PlatForm.CURSEFORGE: {
+                DataType.MC_VERS: list(),
+                DataType.DIR_MAP: dict(),
+                DataType.TYPE_MAP: dict(),
+                DataType.VER_MAP: dict(),
+                DataType.PUBLISH: False
+            }
+        }
+
+    @staticmethod
+    def is_beta(ver: str) -> bool:
+        return bool(Version.coerce(ver).prerelease)
+
+    def run(self):
+        for platform in PlatForm:
+            dirs = [i.name for i in Path(platform).iterdir() if i.joinpath("pack.toml").exists()]
+            for mc_dir in dirs:
+                self.write_data(platform, mc_dir)
+
+            # It is set to true only when there is a version
+            if self.data[platform][DataType.MC_VERS]:
+                self.data[platform][DataType.PUBLISH] = True
+
+        # Set to true when there is a tag that needs to be created
+        if self.data[DataType.CREATE]:
+            self.data[DataType.SHOULD_CREATE] = True
+
+        self.output_json()
+
+    def write_data(self, platform: PlatForm, mc_dir: str):
+        path = Path(platform).joinpath(mc_dir).joinpath("pack.toml")
+        with open(path, "rb") as f:
+            data = tomllib.load(f)
+        mc_ver: str = data["versions"]["minecraft"]
+        pack_ver: str = data["version"]
+        pack_name: str = data["name"]
+
+        # There should be no alpha version
+        if re.match(".*alpha\\.\\d+", pack_ver): raise ValueError
+
+        # skip the cf snapshot version release
+        if platform == PlatForm.CURSEFORGE and self.is_beta(mc_ver): return
+
+        # If it has already been published, don't publish it again
+        end = "mrpack" if platform == PlatForm.MODRINTH else "zip"
+        file_name = f"{pack_name}-{pack_ver}+mc{mc_ver}.{end}"
+        if file_name in self.get_assets(pack_ver): return
+
+        # If the tag doesn't exist, we need to have it pre-created
+        if not self.tag_exists(pack_ver) and not pack_ver in self.data[DataType.CREATE]:
+            self.data[DataType.CREATE].append(pack_ver)
+
+        # write data
+        self.data[platform][DataType.MC_VERS].append(mc_ver)
+        self.data[platform][DataType.DIR_MAP][mc_ver] = mc_dir
+        self.data[platform][DataType.TYPE_MAP][mc_ver] = "beta" if self.is_beta(pack_ver) else "release"
+        self.data[platform][DataType.VER_MAP][mc_ver] = pack_ver
+
+    def output_json(self):
+        with open("data.json", "w", encoding="utf-8") as f:
+            json.dump(self.data, f)
+
+    def read_tags(self) -> list[str]:
+        with Github(auth=self.get_auth()) as g:
+            repo = g.get_repo(self.repository)
+            return [i.tag_name for i in repo.get_releases()]
+
+    def get_assets(self, ver: str) -> list[str]:
+        if ver in self.cache[Cache.ASSETS]:
+            return self.cache[Cache.ASSETS][ver]
+        elif not self.tag_exists(ver):
+            return list()
+        else:
+            tag = f"release/{ver}"
+            with Github(auth=self.get_auth()) as g:
+                repo = g.get_repo(self.repository)
+                assets = [i.name for i in repo.get_release(tag).assets]
+                self.cache[Cache.ASSETS][ver] = assets
+                return assets
+
+    def tag_exists(self, ver: str) -> bool:
+        return f"release/{ver}" in self.cache[Cache.TAGS]
+
+    def get_auth(self) -> Auth.Token | None:
+        if self.token is None:
+            return None
+        else:
+            return Auth.Token(self.token)
 
 
-def get_data(platform: PlatForm) -> dict[str, list[str] | dict[str, str]]:
-    dirs = [i.name for i in Path(platform).iterdir() if i.joinpath('pack.toml').exists()]
-    data = {
-        DataType.MC_VERS: list(),
-        DataType.DIR_MAP: dict(),
-        DataType.TYPE_MAP: dict(),
-        DataType.VER_MAP: dict(),
-        DataType.PUBLISH: False
-    }
-    for mc_dir in dirs:
-        path = Path(platform).joinpath(mc_dir).joinpath('pack.toml')
-        with open(path, 'rb') as f:
-            toml = tomllib.load(f)
-        mc_ver = toml['versions']['minecraft']
-        pack_ver = toml['version']
-        if file_exists(platform, mc_ver, pack_ver): continue
-        data[DataType.MC_VERS].append(mc_ver)
-        data[DataType.DIR_MAP][mc_ver] = mc_dir
-        if re.match('.*alpha\\.\\d+', pack_ver): raise ValueError
-        pack_type = 'beta' if re.match('.*(beta|rc)\\.\\d+', pack_ver) else 'release'
-        data[DataType.TYPE_MAP][mc_ver] = pack_type
-        data[DataType.VER_MAP][mc_ver] = pack_ver
-    if len(data[DataType.MC_VERS]) != 0:
-        data[DataType.PUBLISH] = True
-    return data
-
-
-def file_exists(platform: PlatForm, mc_ver: str, pack_ver: str) -> bool:
-    suffix = {
-        PlatForm.MODRINTH: '.mrpack',
-        PlatForm.CURSEFORGE: '.zip'
-    }[platform]
-    name = re.sub('.*/', '', os.getenv('GITHUB_REPOSITORY', 'DuskScorpio/RedstoneToolkit'))
-    file_name = f'{name}-{pack_ver}+mc{mc_ver}{suffix}'
-    tag = f'release/{pack_ver}'
-    if not tag_exists(tag):
-        return False
-    if pack_ver in CACHE[__CACHE.ASSETS]:
-        return file_name in CACHE[__CACHE.ASSETS][pack_ver]
-    with Github(auth=get_auth()) as g:
-        repo = g.get_repo(os.getenv('GITHUB_REPOSITORY', 'DuskScorpio/RedstoneToolkit'))
-        assets = [i.name for i in repo.get_release(tag).get_assets()]
-        CACHE[__CACHE.ASSETS][pack_ver] = assets
-        return file_name in assets
-
-
-def get_vers() -> list[str]:
-    vers = list()
-    for platform in PlatForm:
-        pack_paths = [i.joinpath('pack.toml') for i in Path(platform).iterdir() if i.joinpath('pack.toml').exists()]
-        for pack_path in pack_paths:
-            with open(pack_path, 'rb') as f:
-                vers.append(tomllib.load(f)['version'])
-    return list(set(vers))
-
-
-def tag_exists(tag: str) -> bool:
-    if CACHE[__CACHE.TAGS] or CACHE['flag']:
-        return tag in CACHE[__CACHE.TAGS]
-    else:
-        with Github(auth=get_auth()) as g:
-            repo = g.get_repo(os.getenv('GITHUB_REPOSITORY', 'DuskScorpio/RedstoneToolkit'))
-            tags = [i.tag_name for i in repo.get_releases()]
-            CACHE[__CACHE.TAGS].extend(tags)
-            CACHE['flag'] = True
-            return tag in tags
-
-
-def get_auth():
-    token = os.getenv('GH_TOKEN')
-    if token is None:
-        return None
-    else:
-        return Auth.Token(token)
-
-if __name__ == '__main__':
-    main()
-    del CACHE
+if __name__ == "__main__":
+    Matrix().run()
